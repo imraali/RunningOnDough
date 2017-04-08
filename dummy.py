fca88ed51d48c918e2b0c3f24d0c3f5d2d4da076
swetlika <swetlika98@gmail.com>
2017-04-08 03:50:41 -0500

    login

---
+++ dummy.py
@@ -0,0 +1,25 @@
+import datetime
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from tabledef import *
+ 
+engine = create_engine('sqlite:///tutorial.db', echo=True)
+ 
+# create a Session
+Session = sessionmaker(bind=engine)
+session = Session()
+ 
+user = User("test","test")
+session.add(user)
+ 
+user = User("test2","test2")
+session.add(user)
+ 
+user = User("test3","test3")
+session.add(user)
+ 
+# commit the record the database
+session.commit()
+ 
+session.commit()
+
